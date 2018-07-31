74c09b5783bc8005bd5ddf4a20ffab10fafda414
Fabio Pinto <fabio.aa.pinto@hotmail.com>
2018-07-31 14:25:25 +0800

    rails new

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+
+  def index         # GET /tasks
+    @tasks = Task.all
+  end
+
+  def show          # GET /tasks/:id
+    @task = Task.find(params[:id])
+
+  end
+
+  def new           # GET /tasks/new
+     @task = Task.new
+  end
+
+  def create        # POST /tasks
+    @task = Task.new(task_params  )
+    @task.save
+  end
+
+  def edit          # GET /tasks/:id/edit
+    @task = Task.find(params[:id])
+  end
+
+  def update        # PATCH /tasks/:id
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy       # DELETE /tasks/:id
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:task).permit(:title, :details)
+  end
+end
